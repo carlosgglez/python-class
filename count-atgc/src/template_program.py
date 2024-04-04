fd1229b0cdd2b327534a80e265b4fbce8b1f4167
carlosgglez <160920668+carlosgglez@users.noreply.github.com>
2024-04-04 12:22:46 -0600

    Update template_program.py

--- count-atgc/src/template_program.py
+++ count-atgc/src/template_program.py
@@ -1,12 +1,12 @@
 '''
-NAME
+Count ATGC
        
-
 VERSION
-        
+
 
 AUTHOR
-        
+
+Carlos García
 
 DESCRIPTION
         
@@ -16,7 +16,7 @@ CATEGORY
 
 USAGE
 
-    % python programName [-parameter] [parameter_value]
+    % python count-atgc [-parameter] [parameter_value]
     
 
 ARGUMENTS
@@ -72,3 +72,26 @@ SEE ALSO
 
 
 
+def contar_ocurrencias_letras(archivo):
+    ocurrencias = {'A': 0, 'T': 0, 'C': 0, 'G': 0}
+    with open(archivo, 'r') as f:
+        for linea in f:
+            for letra in linea:
+                if letra.upper() in ocurrencias:
+                    ocurrencias[letra.upper()] += 1
+    return ocurrencias
+
+def main():
+    nombre_archivo = input("Por favor, introduce el nombre del archivo: ")
+    try:
+        resultado = contar_ocurrencias_letras(nombre_archivo)
+        print("Ocurrencias de las letras en el archivo:")
+        for letra, cantidad in resultado.items():
+            print(f"{letra}: {cantidad}")
+    except FileNotFoundError:
+        print("El archivo no se encontró.")
+    except Exception as e:
+        print("Ocurrió un error:", e)
+
+if __name__ == "__main__":
+    main()
