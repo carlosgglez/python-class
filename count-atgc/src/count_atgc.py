5f0341c74c6ee645d8c8b49136a844fd4c5f9249
beth-la <100377667+beth-la@users.noreply.github.com>
2024-04-10 14:06:14 -0600

    Update count_atgc.py
    Te dejé una versión de código más simple :)
    Muy bien con tu código carlitos

--- count-atgc/src/count_atgc.py
+++ count-atgc/src/count_atgc.py
@@ -55,4 +55,12 @@ def main():
 if __name__ == "__main__":
     main()
 
+# Te dejo una versión de código más simple, aún no hemos visto métodos en clase, pero son una cosa maravillosa que permite que puedas hacer un monton de cosas 
+# En este caso usé el método count:
+with open(archivo, 'r') as f:
+    DNA = f.read()
+# Obtenemos la frecuencia de aparicion de cad aletra.
+print(f"El total por base es: A:{ADN.count('A')} C:{ADN.count('C')} T:{ADN.count('T')} G:{ADN.count('G')}")
+
+# En general tú código está muy bien.
 ```
