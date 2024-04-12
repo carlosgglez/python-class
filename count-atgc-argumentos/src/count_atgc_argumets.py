02f6434198e6db3b0a0dc44d0440ae22a4a0f60b
carlosgglez <carlosgg@lcg.unam.mx>
2024-04-11 18:00:38 -0600

    Se agrego el programa y se lleno el archivo count_atgc_arguments.py

--- count-atgc-argumentos/src/count_atgc_argumets.py
+++ count-atgc-argumentos/src/count_atgc_argumets.py
@@ -1,5 +1,5 @@
 '''
-Count ATGC
+Count ATGC with arguments 
        
 VERSION
 
@@ -14,16 +14,21 @@ Este un  script que cuenta cuantas veces a parece "A", "T", "G", "C" de una secu
 
 CATEGORY
 
+Herramienta que cuenta los nucleótidos de una secuencia de DNA que viene de un archivo.
+
 
 USAGE
 
-    % python count-atgc [-parameter] [parameter_value]
+    % python count-atgc-arguments.py
     
 
 ARGUMENTS
 
+Argumento posicional =  ("input_file", type=str, help ="El archivo de texto que se quiere procesar.")
+Argumento opcional= ("-n", "--nucleotide", type=str, choices=["A","T", "G", "C"], default="ATGC", help='El o los nucleótidos que quieras contar, por default son ATGC')
 
 METHOD
+
 
 
 SEE ALSO
@@ -31,25 +36,43 @@ SEE ALSO
 
 ALL
 
+# ===========================================================================
+# =                           libreria
+# ===========================================================================
 
 import argparse
-
-def main():
-
-        parser = argparse.ArgumentParser(description="Lee el archivo de entrada y salida")
-
-        #Se agrega un argumento posicional para el archivo de entrada.
-        parser.add_argument("input_file", type=str, help ="El archivo de texto que se quiere procesar.")
-
-        #Se agrega un argumento opcional para el archivo de salida.
-        #"out.txt" como nombre de archivo por defecto.
-        parser.add_argument ("o", "--output", type=str, default="out.txt", help='El archivo en el cual se guardara la salida del programa, por defecto se llama "out.txt"')
+#Libreria que da el modulo os que permite trabajar y manipular rutas de archivo>
+import os
 
 
-        args=parser.parse_args()
-        with open(args.input_file, 'r') as f:
-                DNA = f.read()
-        print(f"El total por base es: A:{DNA.count('A')} C:{DNA.count('C')} T:{DNA.count('T')} G:{DNA.count('G')}")
+# ===========================================================================
+# =                            main
+# ===========================================================================
+
+
+parser = argparse.ArgumentParser(description="Lee el archivo de entrada y salida")
+
+#Se agrega un argumento posicional para el archivo de entrada.
+
+parser.add_argument("input_file", type=str, help ="El archivo de texto que se quiere procesar.")
+
+#Se agrega un argumento opcional para que selecione los nuceótidos que eliga el usuario
+#ATGC como los nucleótidos determinados por defecto.
+parser.add_argument ("-n", "--nucleotide", type=str, choices=["A","T", "G", "C"], default="ATGC", help='El o los nucleótidos que quieras contar, por default son ATGC')
+
+
+args=parser.parse_args()
+
+#If utilizado para revisar la existencia o no  del archivo
+if not os.path.exists(args.input_file):
+    print("El archivo de entrada especificado no existe.")
+    exit()
+
+
+with open(args.input_file, 'r') as f:
+        DNA = f.read()
+print(f"El total por base es: A:{DNA.count('A')} C:{DNA.count('C')} T:{DNA.count('T')} G:{DNA.count('G')}")
+
 
 
 ````
