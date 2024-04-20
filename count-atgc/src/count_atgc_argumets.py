d6e3d7e35903afa6b9bc9c3b7b628a36d410719f
carlosgglez <carlosgg@lcg.unam.mx>
2024-04-19 20:08:36 -0600

    Se documento el programa

--- count-atgc/src/count_atgc_argumets.py
+++ count-atgc/src/count_atgc_argumets.py
@@ -62,6 +62,7 @@ parser.add_argument ("-n", "--nucleotide", type=str, choices=["A","T", "G", "C"]
 
 args=parser.parse_args()
 
+#Definición de una excepción especial para el programa
 class FileEmptyError(Exception):
     def __init__(self, input_file):
         self.input_file = input_file
@@ -69,39 +70,36 @@ class FileEmptyError(Exception):
         super().__init__(self.message)
 
 
-
+#Se crean las listas nucleotios, nnucleotidos y caracteres_incorrectos con la finalidad de pode buscar caracteres incorrectos 
 nucleotidos=["A", "T", "G", "C"]
 nnucleotidos=["a", "t", "g", "c"]
 caracteres_incorrectos=[]
 
-
+#Try usado para agregar las excepciones necesarias 
 try:
     with open(args.input_file, 'r') as f:
         DNA = f.read()
 
-    if  DNA==None:
+    if  DNA==None: #Si el archivo esta vacio entra a este if que tiene la función de crea una nueva excepcion
         raise FileEmptyError(f"{args.input_file}")
 
     
-    for nucleotido in DNA:
+    for nucleotido in DNA: #for usado para revisar que todos los caracteres del archivo son correctos, si no es así los guarda en una lista vacia
         if nucleotido not in nucleotidos and nnucleotidos:
             caracteres_incorrectos.append(nucleotido)
 
-    if caracteres_incorrectos:
+    if caracteres_incorrectos: #if usado para imprimir los caracteres incorrectos
         print(f"El archivo {args.input_file} contiene caracteres diferentes: {' '.join(caracteres_incorrectos)}")
-        exit()
+        
     
 
-except FileNotFoundError:
+except FileNotFoundError: #Excepción si no se encuentra el archivo
     print(f"El archivo {args.input_file} no existe")
     exit()
 
-except FileEmptyError as e:
+except FileEmptyError as e: #Excepción creada para archivos vacios
     print (e)
     exit()
-
-except FileExistsError:
-
 
     if args.nucleotide=="A":
         print(f"El total de Adeninas es:{DNA.upper().count('A')}") 
